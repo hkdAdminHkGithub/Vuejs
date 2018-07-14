2bc42dfad963363a42284a3695353ca4271c0c26
hkdark <33818840+hkdAdminHkGithub@users.noreply.github.com>
2018-07-14 14:38:37 +0700

    Update Pagination.vue

--- Pagination.vue
+++ Pagination.vue
@@ -1,150 +1,120 @@
 <style>
-	.disabled{
-		background: #559da3 !important;
-		color: rgb(235, 235, 235) !important;
-		font-weight: bold;
-	}
+.disabled {
+	background: #559da3 !important;
+	color: rgb(235, 235, 235) !important;
+	font-weight: bold;
+}
 </style>
 
 <template>
-	<nav aria-label="Page navigation"  class="col-md-8 offset-2">
-		<ul class="pagination justify-content-center">
-			<li class="page-item"
-					:class="{'disabled': isInFirstPage}"
-			>
-				<a class="page-link"
-					@click="onNavPagintion($event, 1)"
-					aria-label="Go to first page"
-				>
+<nav aria-label="Page navigation" class="col">
+	<ul class="pagination justify-content-center">
+		<li class="page-item" :class="{'disabled': isInFirstPage}">
+			<a class="page-link" @click="onNavPagintion($event, 1)" aria-label="Go to first page">
 					Fisrt Page
 				</a>
-			</li>
+		</li>
 
-			<li class="page-item"
-					:class="{'disabled': isInFirstPage}"
-			>
-				<a class="page-link"
-					@click="onNavPagintion($event, currentPage - 1)"
-				>
-					<span aria-hidden="true">&laquo;</span>
-					<span class="sr-only">Next</span>
-				</a>
-			</li>
+		<li class="page-item" :class="{'disabled': isInFirstPage}">
+			<a class="page-link" @click="onNavPagintion($event, currentPage - 1)">
+				<span aria-hidden="true">
+					<i class="fa fa-caret-square-o-left" aria-hidden="true"></i>
+				</span>
+			</a>
+		</li>
 
-			<li v-for="page in pages" :key="page.name" class="page-item"
-				:class="{ 'disabled' : isPageActive(page.name) }"
-			>
-				<a class="page-link"
-					@click="onNavPagintion($event , page.name)"
-					:class="{ 'disabled' : isPageActive(page.name) }"
-				>
+		<li v-for="page in pages" :key="page.name" class="page-item" :class="{ 'disabled' : isPageActive(page.name) }">
+			<a class="page-link" @click="onNavPagintion($event , page.name)" :class="{ 'disabled' : isPageActive(page.name) }">
 					{{ page.name }}
 				</a>
-			</li>
+		</li>
 
-			<li class="page-item"
-					:class="{'disabled': isInLastPage}"
-			>
-				<a class="page-link"
-					@click="onNavPagintion($event, currentPage + 1)"
-				>
-					<span aria-hidden="true">&raquo;</span>
-					<span class="sr-only">Next</span>
-				</a>
-			</li>
+		<li class="page-item" :class="{'disabled': isInLastPage}">
+			<a class="page-link" @click="onNavPagintion($event, currentPage + 1)">
+				<span aria-hidden="true"><i class="fa fa-caret-square-o-right" aria-hidden="true"></i></span>
+			</a>
+		</li>
 
-			<li class="page-item"
-					:class="{'disabled': isInLastPage}"
-			>
-				<a class="page-link"
-					@click="onNavPagintion($event, totalPages)"
-					aria-label="Go to last page"
-				>
+		<li class="page-item" :class="{'disabled': isInLastPage}">
+			<a class="page-link" @click="onNavPagintion($event, totalPages)" aria-label="Go to last page">
 					Last
 				</a>
-			</li>
-		</ul>
-	</nav>
+		</li>
+	</ul>
+</nav>
 </template>
 
 <script>
 export default {
-	props: [
-		'totalPages',
-		'total',
-		'perPage',
-		'currentPage',
-	],
-	data(){
-		return{
-			maxVisibleButtons:5
+	props: {
+		totalPages: Number,
+		total: Number,
+		perPage: Number,
+		currentPage: Number,
+		maxVisibleButtons: {
+			type: Number,
+			default: 5
 		}
 	},
 
-  computed: {
-    startPage() {
-      if (this.currentPage === 1) {
-        return 1;
-      }
+	computed: {
+		startPage() {
+			const ceilVisible = Math.ceil(this.maxVisibleButtons/2);
+			const floorVisible = Math.floor(this.maxVisibleButtons/2);
+			if (this.currentPage === 1) {
+				return 1;
+			} //first
 
-      if (this.currentPage === this.totalPages) {
-        return this.totalPages - this.maxVisibleButtons + 1;
-      }
+			if (this.currentPage === this.totalPages) {
+				return this.totalPages - this.maxVisibleButtons + 1;
+			}	// last
 
-			if (this.currentPage === 82) {
-				return this.currentPage - 3
+			if (this.currentPage -floorVisible < 1) {
+				return this.currentPage - 1
+			}
+			if (this.totalPages - this.currentPage < floorVisible )
+				return this.currentPage - ceilVisible
+			return this.currentPage -floorVisible // center
+		},
+		endPage() {
+			const ceilVisible = Math.ceil(this.maxVisibleButtons/2);
+			const floorVisible = Math.floor(this.maxVisibleButtons/2);
+
+			if (this.startPage === 1)
+				return this.maxVisibleButtons // first
+
+			if (this.currentPage === this.totalPages)
+				return this.totalPages // last
+
+			if (this.currentPage + floorVisible > this.totalPages) {
+				return this.currentPage + 1
+			}
+			return this.currentPage + floorVisible 	// center
+		},
+		pages() {
+			const range = [];
+
+			for (let i = this.startPage; i <= this.endPage; i += 1) {
+				range.push({
+					name: i,
+					isDisabled: i === this.currentPage
+				});
 			}
 
-			if (this.currentPage > 2) {
-				return this.currentPage - 2
-			}
-
-      return this.currentPage - 1;
-
-    },
-    endPage() {
-      return Math.min(this.startPage + this.maxVisibleButtons - 1, this.totalPages);
-
-    },
-    pages() {
-      const range = [];
-
-      for (let i = this.startPage; i <= this.endPage; i+= 1 ) {
-        range.push({
-          name: i,
-          isDisabled: i === this.currentPage
-        });
-      }
-
-      return range;
-    },
-    isInFirstPage() {
-      return this.currentPage === 1;
-    },
-    isInLastPage() {
-      return this.currentPage === this.totalPages;
-    },
-  },
+			return range;
+		},
+		isInFirstPage() {
+			return this.currentPage === 1;
+		},
+		isInLastPage() {
+			return this.currentPage === this.totalPages;
+		},
+	},
 	methods: {
-		onNavPagintion(ev, page){
+		onNavPagintion(ev, page) {
 			ev.preventDefault();
 			this.$emit('pagechanged', page)
 		},
-		// onClickFirstPage(ev) {
-		// 	this.$emit('pagechanged', 1);
-		// },
-		// onClickPreviousPage() {
-		// 	this.$emit('pagechanged', this.currentPage - 1);
-		// },
-		// onClickPage(page) {
-		// 	this.$emit('pagechanged', page);
-		// },
-		// onClickNextPage() {
-		// 	this.$emit('pagechanged', this.currentPage + 1);
-		// },
-		// onClickLastPage() {
-		// 	this.$emit('pagechanged', this.totalPages);
-		// },
 		isPageActive(page) {
 			return this.currentPage === page;
 		},
